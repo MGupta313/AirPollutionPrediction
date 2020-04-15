5863a9b2973acc7fd0718ece7fa99b0194b54a96
MGupta313 <54957102+MGupta313@users.noreply.github.com>
2020-04-15 15:27:22 -0400

    Update process_pollution.py

--- preprocessing/process_pollution_data.py
+++ preprocessing/process_pollution_data.py
@@ -1,10 +1,61 @@
 import csv
+with open("../data/refined-pollution-data-csv.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    processed_data = {}
+    for row in reader:
+        #for key in row:
+            #print(key)
+        if row['date'] != "NA":
+            year, month, date = row['date'].split("-")
+
+            pruned_data = {
+                    'year': year,
+                    'month': month,
+                    'date': date,
+                    'so2': row['so2'],
+                    'no2': row['no2'],
+                    'rspm': row['rspm'],
+                    'spm': row['spm'],
+                    'pm2_5': row['pm2_5']
+                }
+            # map keys based on location
+            state = row['state']
+            city = row['location']
+            #region = row['type']
+            if state not in processed_data:
+                processed_data[state] = {}
+                processed_data[state][city] = pruned_data
+            else:
+                if city not in processed_data:
+                    processed_data[state][city] = pruned_data
+                else:
+                    processed_data[state][city] = pruned_data
+    #print(processed_data)
+
+    city_level_dict = {}
+    for key1 in processed_data:
+        for key2 in processed_data[key1]:
+            if key2 not in city_level_dict:
+                city_level_dict[key2] = processed_data[key1][key2]
+            else:
+                if processed_data[key1][key2]['year'] == 
+                city_level_dict[key2] += processed_data[key1][key2]
+    print(city_level_dict)
+        
+    #state_level_dict = {}
+    #for key in processed_data:
+        #state_level_dict[key] 
+
+
+
+"""
+import csv
 import pdb
 
 def k():
 
     processed_data = {}
-    with open('../data/pollution-data.csv', newline='') as csvfile:
+    with open('../data/refined-pollution-data.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         i = 0
         for row in reader:
@@ -47,3 +98,4 @@ def k():
 
 if __name__=='__main__':
     print(k())
+"""
